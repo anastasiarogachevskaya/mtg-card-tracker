921bec785c662ed7c8990c3db25939486ccaac03
Anastasia <anastasia.rogachevskaya@aller.com>
2021-09-10 10:13:17 +0300

    download and upload data

--- pages/api/card.ts
+++ pages/api/card.ts
@@ -11,7 +11,6 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const { set, number } = req.query;
-  console.log(req.query)
   const { data } = await axios(`https://api.scryfall.com/cards/${set}/${number}`);
   res.status(200).json(data);
   res.status(200);
