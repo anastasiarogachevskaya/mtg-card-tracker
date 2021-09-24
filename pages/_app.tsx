607e21c758e04eca18369297c95f6a286ffc4152
Anastasia <anastasia.rogachevskaya@aller.com>
2021-09-24 14:16:20 +0300

    NextAuth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
+import { Provider } from 'next-auth/client'
 import Header from '../components/Header/Header'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
   return (
-    <>
+    <Provider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
 export default MyApp
