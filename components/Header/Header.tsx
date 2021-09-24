ce01c93960244745f9a434a3fb8980b4a18e9295
Anastasia <anastasia.rogachevskaya@aller.com>
2021-09-24 14:16:36 +0300

    test pages

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import { useRouter } from 'next/router';
 import SearchForm from '../SearchForm';
+import Nav from './Nav';
 
 const Container = styled.div`
   padding: 1em;
@@ -12,6 +13,7 @@ const Header = () => {
 
   return (
     <Container>
+      <Nav />
       <SearchForm  onSearch={(string: string) => router.push(`/search?q=${string}`)} />
     </Container>
   );
