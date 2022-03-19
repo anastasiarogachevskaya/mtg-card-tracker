79a81171251d18b5ab6a5d0e6493810afc6ad0f1
Anastasia <anastasia.rogachevskaya@aller.com>
2022-03-19 19:46:51 +0200

    eslintrc cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,3 @@
-// module.exports = {
-//   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-//   parserOptions: {
-//     ecmaVersion: 12, // Allows for the parsing of modern ECMAScript features
-//     ecmaFeatures: {
-//       jsx: true, // Allows for the parsing of JSX
-//     },
-//     sourceType: 'module', // Allows for the use of imports
-//     project: './tsconfig.json', // Required for rule generation
-//   },
-//   plugins: ['@typescript-eslint', 'react'],
-//   extends: [
-//     'airbnb',
-//     'airbnb/hooks',
-//     'plugin:react/recommended',
-//     'plugin:@next/next/recommended',
-//     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-//     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-//     'plugin:import/errors',
-//     'plugin:import/warnings',
-//     'plugin:import/typescript',
-//   ],
-//   env: {
-//     es6: true,
-//     node: true,
-//     browser: true,
-//   },
-//   rules: {
-//     'react/prop-types': 'off', // No need since we use TS
-//     'no-console': 'off', // Console.log is allowed for log collection
-//   },
-//   ignorePatterns: [
-//     '.next/',
-//     'node_modules/',
-//   ],
-//   settings: {
-//     react: {
-//       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-//     },
-//   },
-//   overrides: [
-//     // Disable prop spreading for Storybook stories since actions addon needs it
-//     // Disable no-extraneous-dependencies for Storybook stories
-//     {
-//       files: ['**/*/*.stories.[jt]s?(x)'],
-//       rules: {
-//         'react/jsx-props-no-spreading': 'off',
-//         'import/no-extraneous-dependencies': 'off',
-//       },
-//     },
-//   ],
-// };
 module.exports = {
   env: {
     browser: true,
@@ -74,11 +22,6 @@ module.exports = {
     'react',
   ],
   settings: {
-    // Make sure ESLint resolves both JS and TS files correctly
-    // 'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
-    // 'import/parsers': {
-    //   '@typescript-eslint/parser': ['.ts', '.tsx'],
-    // },
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
@@ -105,8 +48,6 @@ module.exports = {
       },
     ],
   },
-  // globals: {
-  // },
   overrides: [
     // Typescript settings
     {
