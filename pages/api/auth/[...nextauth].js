bf5e3e7501fa0667b6f0552f0ac1943bd703d3e4
Anastasia <anastasia.rogachevskaya@aller.com>
2022-03-20 19:09:13 +0200

    FacebookProvider and GoogleProvider out from auth options

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
-import FacebookProvider from 'next-auth/providers/facebook';
 import TwitterProvider from 'next-auth/providers/twitter';
-import GoogleProvider from 'next-auth/providers/google';
+import RedditProvider from "next-auth/providers/reddit";
 import GitHubProvider from 'next-auth/providers/github';
 
 
@@ -16,13 +15,9 @@ const options = {
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET
     }),
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
