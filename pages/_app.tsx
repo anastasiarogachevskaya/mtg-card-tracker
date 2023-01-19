0ca9920f8cd8ac6713ccceface0386c4be505895
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-01-19 20:40:01 +0200

    session update due to next-auth update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,33 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Provider } from 'next-auth/client'
-import { ThemeProvider } from 'styled-components'
+
+import { Session } from 'next-auth';
+import { Provider } from 'next-auth/client';
+import { ThemeProvider } from 'styled-components';
 
 import { useDarkMode } from '../lib/useDarkMode';
 import { UserContextProvider } from '../context/userContext';
 
-import Header from '../components/Header/Header'
+import Header from '../components/Header/Header';
 import { lightTheme, darkTheme, GlobalStyle } from '../theme/themes';
 
-export default function App ({ Component, pageProps }: AppProps) {
-  const [ theme, toggleTheme ] = useDarkMode();
-  const themeMode = theme === 'light' ? lightTheme : darkTheme;
-  const { session } = pageProps;
-  return (
-    <Provider session={session}>
-      <ThemeProvider theme={themeMode}>
-      <GlobalStyle />
-        <UserContextProvider>
-          <Header theme={theme} toggleTheme={toggleTheme} />
-          <Component {...pageProps} />
-        </UserContextProvider>
-      </ThemeProvider>
-    </Provider>
-  )
+export default function App({
+	Component,
+	pageProps,
+}: AppProps<{
+	session: Session;
+}>) {
+	const [theme, toggleTheme] = useDarkMode();
+	const themeMode = theme === 'light' ? lightTheme : darkTheme;
+	return (
+		<Provider session={pageProps.session}>
+			<ThemeProvider theme={themeMode}>
+				<GlobalStyle />
+				<UserContextProvider>
+					<Header theme={theme} toggleTheme={toggleTheme} />
+					<Component {...pageProps} />
+				</UserContextProvider>
+			</ThemeProvider>
+		</Provider>
+	);
 }
